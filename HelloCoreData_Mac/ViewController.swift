6b4d74ae4b6af7f3893035b68a170128fc498897
taka2018 <taka2018@taka2018noMac-mini.local>
2018-12-22 14:41:58 +0900

    Initial Commit

---
+++ HelloCoreData_Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HelloCoreData_Mac
+//
+//  Created by taka2018 on 2018/12/22.
+//  Copyright © 2018 taka2018. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
